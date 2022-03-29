c5794e66e4ca760362df3792b2269a77a054fe25
chris campbell <hopprdev@gmail.com>
2022-03-29 18:36:07 -0400

    Styling for projects section

--- components/layout/navbar/Navbar.js
+++ components/layout/navbar/Navbar.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import DesktopNav from "./desktop/Desktopnav";
+import DesktopNav from "./desktop/DesktopNav";
 import MobileNav from "./mobile/Mobile";
 
 function Navbar() {
