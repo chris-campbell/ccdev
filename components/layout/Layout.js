ffb016480290deff371f6b487fc99b765fa8e3e9
chris campbell <hopprdev@gmail.com>
2022-03-29 18:15:46 -0400

    Adjust file name

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -1,7 +1,7 @@
 import Footer from "./footer/Footer";
 import dynamic from "next/dynamic";
 
-const Navbar = dynamic(() => import("./navbar/NavBar"), { ssr: false });
+const Navbar = dynamic(() => import("./navbar/NavBar"), { ssr: true });
 
 function Layout({ children }) {
   return (
