d0e57f8edeb025b43e60c561291c8884b548e1c4
chris campbell <hopprdev@gmail.com>
2023-09-13 17:33:17 -0400

    Remove spacing

--- pages/articles/[...slug].js
+++ pages/articles/[...slug].js
@@ -13,10 +13,6 @@ function ArticleDetail({ article, host }) {
       <Head>
         {/* Default Meta */}
         <title>2ndPlayer | {metaTitle} </title>
-
-
-        {/* Meta tags for Twitter and WhatsApp (Open Graph) */}
-      
         <meta name="twitter:title" content={metaTitle || ""} />
         <meta name="twitter:description" content={metaDescription || ""} />
         <meta name="twitter:image" content={metaImage ? metaImage.url : null} />
@@ -24,31 +20,11 @@ function ArticleDetail({ article, host }) {
         <meta name="twitter:site" content="@2ndplayerco" />
         <meta name="twitter:creator" content="@2ndplayerco" />
         
-        
         <meta property="og:title" content={metaTitle || ""} />
         <meta property="og:description" content={metaDescription || ""} />
         <meta property="og:image" content={metaImage ? metaImage.url : ''} />
         <meta property="og:url" content={`https://${host}${path}`} />
         <meta property="og:site_name" content="@2ndplayerco" />
-    
-
-        {/* <meta property="og:type" content="article" />
-        <meta property="og:image" content={metaImage ? metaImage.url : ''} />
-        <meta property="og:title" content={metaTitle || ''} />
-        <meta property="og:url" content={`https://${host}${path}`} />
-        <meta property="og:locale" content="en_US" />
-        <meta property="og:image" itemProp="image" content={metaImage ? metaImage.url : ''} />
-        <meta property="og:image:width" content="1200" />
-        <meta property="og:image:height" content="630" />
-        <meta property="og:image:secure_url" content={metaImage ? metaImage.url : ''} />
-        <meta property="og:site_name" content="2ndPlayer" />
-
-
-        <meta name="twitter:title" content={metaTitle || ""}/>
-        <meta name="twitter:description" content={metaDescription || ""} />
-        <meta name="twitter:image:src" content={metaImage ? metaImage.url : null} />
-        <meta name="twitter:card" content="summary" />
-        <meta name="twitter:site" content="@2ndplayerco" /> */}
       </Head>
 
       <ArticleDetails article={article} host={host} />
